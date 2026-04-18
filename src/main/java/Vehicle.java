65aeebe4849a0b804f42d5b82b15b52abb542c14
Larry <larryle7680@gmail.com>
2026-04-17 22:02:21 -0500

    Add a constructor. Added a Setter and Getter.

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
-
 public class Vehicle {
+    //Attributes for Vehicle
+    long vehicleId = 0;
+    String makeModel = "";
+    String color = "";
+    int odometerReading = 0;
+    float price = 0;
+
+
+    //Constructor
+    public Vehicle(long vehicleId, String makeModel, String color, int odometerReading, float price) {
+        this.vehicleId = vehicleId;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
+    }
+
+    //Setters and Getters
+    public long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }

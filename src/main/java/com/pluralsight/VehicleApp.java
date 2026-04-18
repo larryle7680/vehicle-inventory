1aec37aaba7f69dd7b8452290ae2fc0a4254c731
Larry <larryle7680@gmail.com>
2026-04-18 11:52:35 -0500

    Created a loop to ask questions about the menu

--- src/main/java/com/pluralsight/VehicleApp.java
+++ src/main/java/com/pluralsight/VehicleApp.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 public class VehicleApp {
     static Scanner theScanner = new Scanner(System.in);
     //create variables to use
-    static double vehicleId = 0;
+    static long vehicleId = 0;
     static String make = "";
     static String color = "";
     static int odometerReading = 0;
     static float price = 0;
 
+    //Creating an Array that passes 20 cars
+    static Vehicle[] vehicle = new Vehicle[20];
+    static int vehicleCounter = 0;
+
     public static void main(String[] args) {
-        //Creating an Array that passes 20 cars
-        Vehicle[] vehicle = new Vehicle[20];
 
         //Now fill the array 6 spots with cars
         new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
@@ -23,13 +25,33 @@ public class VehicleApp {
         new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
     }
 
+    //Creating a method to help loop the questions
+    public static void menuLoop(){
 
+        //Using a loop to ask the questions
+        for(int i = 0; i < vehicle.length; i++ ){
+            //Prompt the user to press a button
+            System.out.println("===Please type in a number===");
+            System.out.println("-----------------------------");
+            System.out.println("1. List all vehicle");
+            System.out.println("2. Search by make/model");
+            System.out.println("3. Search by price range");
+            System.out.println("4. Search by color");
+            System.out.println("5. Add a vehicle");
+            System.out.println("6. Quit");
+            System.out.println("Please enter your command");
 
+            //store the users answer
+             int usersChoice = theScanner.nextInt();
 
-    public static void askQuestions() {
-        System.out.print("What is your com.pluralsight.Vehicle ID?");
-       vehicleId = theScanner.nextLong();
-       vehicleId = setVehicleId(vehicleId);
+             switch()
+
+        }
 
     }
+
+
+
+
+
 }

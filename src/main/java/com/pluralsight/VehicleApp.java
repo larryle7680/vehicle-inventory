f6de0037f078ab0c7291796308fe69b551140d2f
Larry <larryle7680@gmail.com>
2026-04-19 11:03:26 -0500

    Created a counter to check how many things are in the array. Also, finish creating a loop that iterate through the array to find an empty spot to add a new vehicle.

--- src/main/java/com/pluralsight/VehicleApp.java
+++ src/main/java/com/pluralsight/VehicleApp.java
@@ -17,70 +17,82 @@ public class VehicleApp {
 
     public static void main(String[] args) {
 
-           //Now fill the array 6 spots with cars and store it in the array
-           vehicle[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
-           vehicle[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
-           vehicle[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
-           vehicle[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
-           vehicle[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
-           vehicle[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
+        //Now fill the array 6 spots with cars and store it in the array
+        vehicle[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+        vehicle[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+        vehicle[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+        vehicle[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+        vehicle[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+        vehicle[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
 
+        for(int i = 0; i < vehicle.length; i++){
+            if (vehicle[i] != null ){
+                vehicleCounter++;
+            }
+        }
 
-           //Calling the loop
-            menuLoop();
+        //Making Space for clarity
+        System.out.println();
+        System.out.println("There are currently " + vehicleCounter + " Vehicles in the inventory.");
+        System.out.println();
+
+        //Calling the loop
+        menuLoop();
     }
 
     //Creating a method to help loop the questions
-    public static void menuLoop(){
+    public static void menuLoop() {
+        boolean running = true;
+        while (running) {
+            //Using a loop to ask the questions
+            for (int i = 0; i < vehicle.length; i++) {
+                //Prompt the user to press a button
+                System.out.println("===Please type in a number===");
+                System.out.println("-----------------------------");
+                System.out.println("1. List all vehicle");
+                System.out.println("2. Search by make/model");
+                System.out.println("3. Search by price range");
+                System.out.println("4. Search by color");
+                System.out.println("5. Add a vehicle");
+                System.out.println("6. Quit");
+                System.out.println();
+                System.out.println("Please enter your command");
+                System.out.println();
 
-        //Using a loop to ask the questions
-        for(int i = 0; i < vehicle.length; i++ ){
-            //Prompt the user to press a button
-            System.out.println("===Please type in a number===");
-            System.out.println("-----------------------------");
-            System.out.println("1. List all vehicle");
-            System.out.println("2. Search by make/model");
-            System.out.println("3. Search by price range");
-            System.out.println("4. Search by color");
-            System.out.println("5. Add a vehicle");
-            System.out.println("6. Quit");
-            System.out.println();
-            System.out.println("Please enter your command");
-            System.out.println();
+                //store the users answer
+                int usersChoice = theScanner.nextInt();
 
-            //store the users answer
-             int usersChoice = theScanner.nextInt();
+                switch (usersChoice) {
+                    case 1:
+                        displayCarsID();
+                        break;
+                    case 2:
+                        displayMakeModel();
+                        break;
+                    case 3:
 
-             switch(usersChoice){
-                 case 1:
-                     displayCarsID();
-                     break;
-                 case 2:
-                     displayMakeModel();
-                     break;
-                 case 3:
+                        break;
+                    case 4:
 
-                     break;
-                 case 4:
+                        break;
+                    case 5:
+                        addVehicle();
 
-                     break;
-                 case 5:
-                     addVehicle();
-
-                     break;
-                 case 6:
-                     break;
+                        break;
+                    case 6:
+                        return;
 
 
-             }
+                }
+            }
+
         }
-
     }
 
     //Creating a method to display vehicleId
-    public static void displayCarsID(){
+    public static void displayCarsID() {
         for (Vehicle id : vehicle) {
-            if( id != null){
+            if (id != null) {
                 System.out.println(id.getVehicleId());
             }
 
@@ -88,22 +100,26 @@ public class VehicleApp {
     }
 
     //Creating method to display Make/Model
-    public static void displayMakeModel(){
-        for (Vehicle makeModel : vehicle){
+    public static void displayMakeModel() {
+        for (Vehicle makeModel : vehicle) {
             System.out.println(makeModel.getMakeModel());
         }
     }
 
     //Creating a method to add vehicles
-    public static void addVehicle(){
+    public static void addVehicle() {
         //using a for loop because I need to modify the array
-        for (int i = 0; i < vehicle.length; i++)
-            if(vehicle == null){
+        for (int i = 0; i < vehicle.length; i++) {
+            if (vehicle[i] == null) {
                 System.out.print("---Please fill out these option to add a new vehicle---");
                 System.out.println();
                 //Now asking questions for new vehicle and storing it
                 System.out.print("What is your Vehicle ID?\n");
-                double newVehicleID = theScanner.nextDouble();
+                long newVehicleID = theScanner.nextLong();
+
+                //Eat a new line
+                theScanner.nextLine();
+
                 System.out.print("What is the make?\n");
                 String newMakeModel = theScanner.nextLine();
                 System.out.print("What is the color?\n");
@@ -111,15 +127,17 @@ public class VehicleApp {
                 System.out.print("What is number on the Odometer Reading?\n");
                 int newOdometerReading = theScanner.nextInt();
                 System.out.print("What is the price?\n");
-                double newPrice = theScanner.nextDouble();
+                float newPrice = theScanner.nextFloat();
+
+                //Store the answer in a new array
+                vehicle[i] = new Vehicle(newVehicleID, newMakeModel, newColor, newOdometerReading, newPrice);
+                //break to stop the loop
+                break;
+            }
+
 
         }
+
+
     }
-
-
-
-
-
-
-
 }

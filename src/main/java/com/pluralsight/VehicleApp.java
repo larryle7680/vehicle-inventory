e75e0657320e611e2c262cf7c0c2f043dc182060
Larry <larryle7680@gmail.com>
2026-04-18 21:44:58 -0500

    Working on addVehicle method. Asked user for details and storing their answer in the variable.

--- src/main/java/com/pluralsight/VehicleApp.java
+++ src/main/java/com/pluralsight/VehicleApp.java
@@ -25,6 +25,8 @@ public class VehicleApp {
            vehicle[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
            vehicle[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
 
+
+           //Calling the loop
             menuLoop();
     }
 
@@ -42,7 +44,9 @@ public class VehicleApp {
             System.out.println("4. Search by color");
             System.out.println("5. Add a vehicle");
             System.out.println("6. Quit");
+            System.out.println();
             System.out.println("Please enter your command");
+            System.out.println();
 
             //store the users answer
              int usersChoice = theScanner.nextInt();
@@ -52,7 +56,20 @@ public class VehicleApp {
                      displayCarsID();
                      break;
                  case 2:
+                     displayMakeModel();
+                     break;
+                 case 3:
 
+                     break;
+                 case 4:
+
+                     break;
+                 case 5:
+                     addVehicle();
+
+                     break;
+                 case 6:
+                     break;
 
 
              }
@@ -60,14 +77,46 @@ public class VehicleApp {
 
     }
 
-    //Creating a method to display all the cars
+    //Creating a method to display vehicleId
     public static void displayCarsID(){
-        for (Vehicle value : vehicle) {
-            System.out.println(value.getMakeModel());
+        for (Vehicle id : vehicle) {
+            if( id != null){
+                System.out.println(id.getVehicleId());
+            }
+
         }
     }
 
-    //Creating a method
+    //Creating method to display Make/Model
+    public static void displayMakeModel(){
+        for (Vehicle makeModel : vehicle){
+            System.out.println(makeModel.getMakeModel());
+        }
+    }
+
+    //Creating a method to add vehicles
+    public static void addVehicle(){
+        //using a for loop because I need to modify the array
+        for (int i = 0; i < vehicle.length; i++)
+            if(vehicle == null){
+                System.out.print("---Please fill out these option to add a new vehicle---");
+                System.out.println();
+                //Now asking questions for new vehicle and storing it
+                System.out.print("What is your Vehicle ID?\n");
+                double newVehicleID = theScanner.nextDouble();
+                System.out.print("What is the make?\n");
+                String newMakeModel = theScanner.nextLine();
+                System.out.print("What is the color?\n");
+                String newColor = theScanner.nextLine();
+                System.out.print("What is number on the Odometer Reading?\n");
+                int newOdometerReading = theScanner.nextInt();
+                System.out.print("What is the price?\n");
+                double newPrice = theScanner.nextDouble();
+
+        }
+    }
+
+
 
 
 
